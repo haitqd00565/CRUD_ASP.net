f13faf7027539312d8255f77ee852fc10167ffa7
haitqd00565 <haitqd00565@fpt.edu.vn>
2018-11-10 09:28:21 +0700

    hoan thanh

---
+++ CRUD_Video1/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CRUD_Video1.Models;
+
+namespace CRUD_Video1.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
